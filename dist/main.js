8b24c79aef293241386ab77a4693873ff86e764f
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-02-11 18:35:47 +0100

    finished styling, started typescript logic

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,15 @@
 "use strict";
+// <i class="fa-solid fa-square-check" id="check-toggle"></i>
+//import { v4 as uuidv4 } from 'uuid';
+const btnAdd = document.getElementById("btn-add");
+let savedTodo = [];
+function createTodo() {
+    let inputAdd = document.getElementById("input-add");
+    if (inputAdd instanceof HTMLInputElement && inputAdd.value.trim() !== "") {
+        console.log("start");
+    }
+    else {
+        console.log("No input provided!");
+    }
+}
+btnAdd === null || btnAdd === void 0 ? void 0 : btnAdd.addEventListener("click", createTodo);
