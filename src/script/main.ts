8e2780306eab60412b977a2fa5064124198d5ec3
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-02-12 14:08:22 +0100

    created a function that pushes saved todos in it, created a function that updates the page

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,20 +1,53 @@
 // <i class="fa-solid fa-square-check" id="check-toggle"></i>
-
-//import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const btnAdd = document.getElementById("btn-add");
-let savedTodo = []; 
+const savedTodo:todoTypes[] = [];
 
-function createTodo() {
+type todoTypes = {
+    id: string,
+    title: string,
+}
+
+function createTodo():void {
     let inputAdd = document.getElementById("input-add");
 
     if (inputAdd instanceof HTMLInputElement && inputAdd.value.trim() !== "") {
-        console.log("start");
-        
+        const newTodo: todoTypes = ({
+            id: uuidv4(),
+            title: inputAdd.value,
+        });
+        savedTodo.push(newTodo);
+        console.log(savedTodo)
+        updateTodo(inputAdd)
     } else {
-        console.log("No input provided!")
+        alert("No input entered.")
     }
 }
 
+function updateTodo(inputAdd:HTMLInputElement):void {
+    let todoList = document.querySelector(".todo-list")!;
+
+    if (savedTodo.length === 0) {
+        todoList.innerHTML = `<h1>no saved todo items</h1>`;
+    } else {
+        todoList.innerHTML = '';
+        savedTodo.forEach((todo) => {
+            todoList.innerHTML += 
+            `<div class="todo-box" id="${todo.id}">
+                <i class="fa-regular fa-square" id="check-toggle"></i>
+                <input type="text" id="check-text" value="${todo.title}" readonly>
+                <i class="fa-solid fa-pen-to-square" id="check-edit"></i>
+                <i class="fa-regular fa-circle-xmark" id="check-remove"></i>
+            </div>`
+        });
+    
+        inputAdd.value = '';
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    updateTodo
+});
 
 btnAdd?.addEventListener("click" , createTodo);
